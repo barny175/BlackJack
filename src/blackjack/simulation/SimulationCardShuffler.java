43e1ab8d7dfde417c2c48769b999b4a3bab5170b
Martin Barnas <mbarnas@kerio.com>
2012-10-15 13:26:38 +0200

    simulation card shuffler implementation

--- src/blackjack/simulation/SimulationCardShuffler.java
+++ src/blackjack/simulation/SimulationCardShuffler.java
@@ -4,10 +4,32 @@
  */
 package blackjack.simulation;
 
+import blackjack.engine.Card;
+import blackjack.engine.CardShuffler;
+import java.util.LinkedList;
+
 /**
  *
  * @author mbarnas
  */
-public class SimulationCardShuffler {
+public class SimulationCardShuffler extends CardShuffler {
+	private final Card[] firstCards;
+	private int cardsUsed;
+
+	public SimulationCardShuffler(int decks, Card... firstCards) {
+		super(decks);
+		this.firstCards = firstCards;
+	}
 	
+	
+    @Override
+    public Card next() {
+		if (cardsUsed < firstCards.length) {
+			Card c = firstCards[cardsUsed++];
+			this.used.add(c.ordinal());
+			return c;
+		}
+		
+		return super.next();
+	}
 }
