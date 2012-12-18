95fcb31ad957a05597b14c93329a6b8398779044
Martin Barnas <mbarnas@kerio.com>
2012-12-18 15:35:07 +0100

    +test for splitting in basic rules

---
+++ test/blackjack/engine/rules/BasicRulesTest.groovy
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package blackjack.engine.rules
+
+import org.junit.Test
+import static org.junit.Assert.*
+import blackjack.engine.*
+import static org.mockito.Mockito.*
+import org.mockito.*
+import static blackjack.Utils.*
+
+/**
+ *
+ * @author mbarnas
+ */
+class BasicRulesTest {
+	@Test
+	void splitOnlySameCards() {
+		def player = mock(Player.class)
+		def engine = mock(Engine.class)
+		
+		def game = new Game(engine, player)
+		game.addPlayerCard(Card.THREE)
+		game.addPlayerCard(Card.THREE)
+		game.setGameState(GameState.FirstDeal)
+
+		def rules = new BasicRules()		
+		def allowedMoves = rules.getAllowedMoves(game)
+		assertTrue(allowedMoves.contains(Move.Split))
+	}
+	
+	@Test
+	void dontAllowSplitOfDifferentCards() {
+		def player = mock(Player.class)
+		def engine = mock(Engine.class)
+		
+		def game = new Game(engine, player)
+		game.addPlayerCard(Card.TWO)
+		game.addPlayerCard(Card.THREE)
+		game.setGameState(GameState.FirstDeal)
+
+		def rules = new BasicRules()		
+		def allowedMoves = rules.getAllowedMoves(game)
+		assertFalse(allowedMoves.contains(Move.Split))
+	}
+}
+
