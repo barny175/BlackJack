d93adf94514331c4cd7a23ef1c5f642895fb9dbe
mbarnas <mbarnas@kerio.com>
2013-01-01 16:54:10 +0100

    missing file

---
+++ src/blackjack/engine/rules/DoubleAfterSplit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack.engine.rules;
+
+import com.google.inject.BindingAnnotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author mbarnas
+ */
+@Retention(RetentionPolicy.RUNTIME) 
+@Target({ElementType.PARAMETER})    
+@BindingAnnotation
+public @interface DoubleAfterSplit {
+	
+}
